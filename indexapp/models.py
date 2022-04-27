2e5f5183d5b4fec84b2f2954ab508c1ce37e453a
Josue Reyes <josueisrae222@gmail.com>
2022-04-27 12:23:10 -0500

    Models para la base de datos

--- indexapp/models.py
+++ indexapp/models.py
@@ -1,3 +1,24 @@
+
 from django.db import models
 
 # Create your models here.
+
+class Marca_Producto(models.Model):
+    nombre_marca=models.CharField(max_length=30)
+
+class Categoria_Producto(models.Model):
+    nombre_categ=models.CharField(max_length=30)
+
+class Producto(models.Model):
+    nombre=models.CharField(max_length=50)
+    precio=models.IntegerField()
+    descripcion=models.CharField(max_length=100)
+    fecha_fabricacion=models.DateField()
+    marca=models.ForeignKey(Marca_Producto,on_delete=models.CASCADE)
+    categoria=models.ForeignKey(Categoria_Producto, on_delete=models.CASCADE)
+#     # imagen=models.ImageField(update_to="productos", null=True)
+
+class ImagenProducto(models.Model):
+    imagen=models.ImageField(upload_to="producto")
+    Producto=models.ForeignKey(Producto, on_delete=models.CASCADE)
+
