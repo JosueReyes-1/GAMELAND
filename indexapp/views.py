a31919d892cfa7da57dfa714891289681016e88b
Josue Reyes <josueisrae222@gmail.com>
2022-05-27 01:10:09 -0500

    se validaron los posibles errores de consulta tanto de backend como de frontend

--- indexapp/views.py
+++ indexapp/views.py
@@ -1,10 +1,9 @@
-from multiprocessing import context
-from queue import Empty
-from urllib import request
+
 from django.shortcuts import render,HttpResponse
 from indexapp.models import Producto,ImagenProducto
 from django.core.paginator import Paginator,EmptyPage
-# Create your views here.
+
+
 
 def index(request):
     # consulta de productos nuevos
@@ -12,7 +11,7 @@ def index(request):
     imagenesP=ImagenProducto.objects.all()
     # consulta de productos en descuentos
     productos_desc=Producto.objects.filter(estado__estado='descuento')
-
+    
     context={
         "nuevos": productos_nuevos,
         "desc": productos_desc,
@@ -21,15 +20,15 @@ def index(request):
     return render(request,"indexapp/index.html", context)
 
 
-
 def detalles(request, slug_text):
     productos=Producto.objects.filter(slug=slug_text)
     imagenesP=ImagenProducto.objects.filter(producto__slug=slug_text) 
 
-    if productos.exists():
+    if productos.exists() and imagenesP.exists():
         productos=productos.first()
     else:
-        return HttpResponse("<h5>Pagina No encontrada</h5>")
+        pass
+        # redirigir a pestaña de error
     
     context={
         'producto':productos,
@@ -38,10 +37,12 @@ def detalles(request, slug_text):
     
     return render(request,'indexapp/detalles.html',context)
 
+
 def categorias(request, slug_text):
+    
     productos=Producto.objects.filter(categoria__slug=slug_text)
     imagenesP=ImagenProducto.objects.all()
-
+    
     p=Paginator(productos,10)
     page_num=request.GET.get('page',1)
 
@@ -49,6 +50,8 @@ def categorias(request, slug_text):
         page=p.page(page_num)
     except EmptyPage:
         page=p.page(1)
+
+
     context={
         'productos':page,
         'imagenes':imagenesP,
